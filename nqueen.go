55f4029f03fc616bb2b606ee7e131a8bff145f96
Ilya Kaliman <ilya.kaliman@gmail.com>
2018-02-07 11:27:00 -0800

    N-Queens problem solutions using backtracking.

---
+++ nqueen.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func checkij(board [][]bool, i, j int) bool {
+	n := len(board)
+	for d := 1; i+d < n; d++ {
+		if board[i+d][j] {
+			return false
+		}
+	}
+	for d := 1; j+d < n; d++ {
+		if board[i][j+d] {
+			return false
+		}
+	}
+	for d := 1; i+d < n && j+d < n; d++ {
+		if board[i+d][j+d] {
+			return false
+		}
+	}
+	for d := 1; i-d >= 0 && j+d < n; d++ {
+		if board[i-d][j+d] {
+			return false
+		}
+	}
+	return true
+}
+
+func checkboard(board [][]bool) bool {
+	n := len(board)
+	for row := 0; row < n; row++ {
+		for col := 0; col < n; col++ {
+			if board[row][col] && !checkij(board, row, col) {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func printboard(board [][]bool) {
+	n := len(board)
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			if board[i][j] {
+				fmt.Print("q")
+			} else {
+				fmt.Print("x")
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func solve(board [][]bool, row int) bool {
+	n := len(board)
+	if row == n {
+		printboard(board)
+		fmt.Println()
+		return true
+	}
+	found := false
+	for col := 0; col < n; col++ {
+		board[row][col] = true
+		if checkboard(board) && solve(board, row+1) {
+			found = true
+		}
+		board[row][col] = false
+	}
+	return found
+}
+
+func nqueen(n int) {
+	board := make([][]bool, n)
+	for i := range board {
+		board[i] = make([]bool, n)
+	}
+	if !solve(board, 0) {
+		fmt.Println("no solutions found")
+	}
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("specify n")
+		return
+	}
+	n, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if n < 1 {
+		fmt.Println("n must be positive")
+		return
+	}
+	nqueen(n)
+}
